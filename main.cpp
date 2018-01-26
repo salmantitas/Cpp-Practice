a7e2372a13951ecc21c60a476b0117d78739d87d
salmantitas <salman.mehedy@gmail.com>
2018-01-26 11:46:26 -0800

    Add files via upload
    version #2

--- main.cpp
+++ main.cpp
@@ -2,13 +2,29 @@
 
 using namespace std;
 
-int scoreNeededForAverage(int takenSoFar, int currentAverage, int taking, int wantedAverage) {
+ /* Returns the required score per course for desired average
+  * (currentAverage*takenSoFar + taking*requiredPerCourse)/(takensoFar + taking ) = newAverage
+  */
+int scoreNeededForAverage(int takenSoFar, int currentAverage, int taking, int wantedAverage)
+{
     return ((wantedAverage * (takenSoFar + taking)) - currentAverage*takenSoFar)/taking;
 }
 
 int main()
 {
-    int required = scoreNeededForAverage(8, 72, 5, 73);
+    cout << "Enter the number of courses you have taken: ";
+    int a;
+    cin >> a;
+    cout << "Enter your current average: ";
+    int b;
+    cin >> b;
+    cout << "Enter the number of courses you are taking: ";
+    int c;
+    cin >> c;
+    cout << "Enter your desired average: ";
+    int d;
+    cin >> d;
+    int required = scoreNeededForAverage(a, b, c, d);
     if (required > 100) {
         cout << "An average of that score cannot be reached at the moment." << endl;
     }
@@ -17,8 +33,6 @@ int main()
     }
     return 0;
 }
- /* Returns the required score per course for desired average
-  * (currentAverage*takenSoFar + taking*requiredPerCourse)/(takensoFar + taking ) = newAverage
-  */
+
 
 
